7ff13885a05126b543063c907997cda66f42bce5
Lohit Bura <lohitbura@gmail.com>
2021-12-22 21:53:24 +0530

    master

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component{
     }
 }
 
-class Board extends react.Component{
+class Board extends React.Component{
     renderSquare(i){
         return <Square value = {i} />
     }
@@ -21,14 +21,44 @@ class Board extends react.Component{
     render(){
         return (
             <div>
+                <div className="status">Player Turn : {this.props.status}</div>
                 <div className="board-row">
-                        
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                        {this.renderSquare(3)}
+                     </div> 
+                     <div className="board-row">
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                        {this.renderSquare(6)}
+                     </div> 
+                     <div className="board-row">
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                        {this.renderSquare(9)}
                      </div> 
              </div>
         );
     }
 }
 
-class Game extends react.Component{
+class Game extends React.Component{
+    render(){
+      return (
+          <div className="game">
+            <div className="game-board">
+                <Board status={"X"}/>
+            </div>
+            <div className="game-info">
+
+            </div>
+          </div>
+      );
+}
 
 }
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById("root")
+);
